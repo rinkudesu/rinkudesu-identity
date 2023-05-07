8014b7cc2dedccd7fa4b586a8fe264910ecbaaa9
Piotr Kowalski <piotr@pkowalski.net>
2023-05-07 12:38:25 +0200

    Improve the JWT controller

--- Rinkudesu.Identity.Service/Controllers/JwtController.cs
+++ Rinkudesu.Identity.Service/Controllers/JwtController.cs
@@ -1,82 +1,89 @@
-﻿//todo: this entire controller has been commented out as it prevented the program from building on release - it will be restored in a sane state soon
-// using Microsoft.AspNetCore.Authorization;
-// using Microsoft.AspNetCore.Mvc;
-// using System.IdentityModel.Tokens.Jwt;
-// using System.Security.Claims;
-// using System.Security.Cryptography;
-// using System.Text.Json;
-// using System.Text.Json.Serialization;
-// using Microsoft.AspNetCore.Identity;
-// using Microsoft.IdentityModel.Tokens;
-// using Rinkudesu.Identity.Service.Models;
-//
-// namespace Rinkudesu.Identity.Service.Controllers;
-//
-// [ApiController, Route("api/[controller]")]
-// public class JwtController : ControllerBase
-// {
-//     private readonly UserManager<User> _userManager;
-//     private readonly JwtSecurityTokenHandler _tokenHandler = new JwtSecurityTokenHandler();
-//
-//     //todo: this is so temp it physically hurts
-//     private static JsonWebKey? jsonWebKey;
-//     private static RSA rsa;
-//
-//     public static JsonWebKey JWK {
-//         get
-//         {
-//             if (jsonWebKey is not null)
-//                 return jsonWebKey;
-//             rsa = RSA.Create(2048);
-//             jsonWebKey = new JsonWebKey
-//             {
-//                 KeyId = Guid.NewGuid().ToString(),
-//                 Use = "sig",
-//                 Alg = "RSA256",
-//                 Kty = "RSA",
-//                 E = Base64UrlEncoder.Encode(rsa.ExportParameters(false).Exponent),
-//                 N = Base64UrlEncoder.Encode(rsa.ExportParameters(false).Modulus),
-//             };
-//             return jsonWebKey;
-//         }
-//     }
-//
-//     public JwtController(UserManager<User> userManager)
-//     {
-//         _userManager = userManager;
-//     }
-//
-//     [HttpGet("jwks.json")]
-//     public ActionResult Jwsk()
-//     {
-//         return Content($"{{\"keys\":[{System.Text.Json.JsonSerializer.Serialize(JWK, new JsonSerializerOptions{DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull, PropertyNamingPolicy = JsonNamingPolicy.CamelCase})}]}}","application/json");
-//     }
-//
-//     [HttpGet, Authorize]
-//     public async Task<ActionResult> IssueJwt()
-//     {
-//         var user = await _userManager.FindByNameAsync(User.Identity.Name);
-//         var key = "move this thing to a config file you baka"u8.ToArray();
-//
-//         var claims = new ClaimsIdentity(new Claim[]
-//         {
-//             new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-//         });
-//
-//         _ = JWK;
-//         var sk = new RsaSecurityKey(rsa);
-//
-//         var tokenDescriptor = new SecurityTokenDescriptor
-//         {
-//             Subject = claims,
-//             Expires = DateTime.UtcNow.AddMinutes(10),
-//             Audience = "rinkudesu",
-//             Issuer = "http://localhost:5500/",
-//             SigningCredentials = new SigningCredentials(sk, SecurityAlgorithms.RsaSha256),
-//         };
-//
-//         var token = _tokenHandler.CreateToken(tokenDescriptor);
-//         var tokenString = _tokenHandler.WriteToken(token);
-//         return Ok(tokenString);
-//     }
-// }
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using Rinkudesu.Identity.Service.Models;
+using Rinkudesu.Identity.Service.Repositories;
+using Rinkudesu.Identity.Service.Utilities;
+
+namespace Rinkudesu.Identity.Service.Controllers;
+
+/// <summary>
+/// Controller responsible for JWT management, providing endpoints required for issuing and verifying tokens.
+/// </summary>
+[ApiController]
+[ApiVersion("v1"), Route("api/[controller]"), Route("api/v{version:apiVersion}/[controller]")]
+public class JwtController : ControllerBase
+{
+    private readonly UserManager<User> _userManager;
+    private readonly JwtKeysRepository _jwtKeysRepository;
+    private readonly JwtSecurityTokenHandler _tokenHandler;
+
+    private static readonly JsonSerializerOptions jwksSerializerOptions = new JsonSerializerOptions
+    {
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    };
+
+    /// <inheritdoc />
+    public JwtController(UserManager<User> userManager, JwtKeysRepository jwtKeysRepository, JwtSecurityTokenHandler tokenHandler)
+    {
+        _userManager = userManager;
+        _jwtKeysRepository = jwtKeysRepository;
+        _tokenHandler = tokenHandler;
+    }
+
+    /// <summary>
+    /// Returns a openid-connect compliant list of Json Web Keys used for JWT signing. The response is in json format.
+    /// </summary>
+    [HttpGet("jwks.json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public ActionResult Jwsk()
+    {
+        var keysJson = $"{{\"keys\":[{JsonSerializer.Serialize(_jwtKeysRepository.GetRsaAsJsonWebKey(), jwksSerializerOptions)}]}}";
+        return Content(keysJson,"application/json");
+    }
+
+    /// <summary>
+    /// Generates a new JWT for the current user. The token is valid for 10 minutes.
+    /// </summary>
+    /// <returns>A valid JWT returned as string.</returns>
+    /// <remarks>
+    /// This token should never really be presented to the user directly. It should only be used for back-end authorisation.
+    /// For front-end tokens, the identity cookie should be used.
+    /// </remarks>
+    /// <response code="404">When user is not set or cannot be found.</response>
+    [HttpGet, Authorize]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> IssueJwt()
+    {
+        if (User.Identity?.Name is null)
+            return NotFound();
+        var user = await _userManager.FindByNameAsync(User.Identity.Name);
+        if (user is null)
+            return NotFound();
+
+        var claims = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+        });
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = claims,
+            Expires = DateTime.UtcNow.AddMinutes(10),
+            Audience = "rinkudesu",
+            Issuer = EnvironmentalVariablesReader.GetBaseUrl(),
+            SigningCredentials = _jwtKeysRepository.GetRsaAsSigningCredentials(),
+        };
+
+        var token = _tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = _tokenHandler.WriteToken(token);
+        return Ok(tokenString);
+    }
+}
---
+++ Rinkudesu.Identity.Service/Repositories/JwtKeysRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+using Microsoft.IdentityModel.Tokens;
+using Rinkudesu.Identity.Service.Utilities;
+
+namespace Rinkudesu.Identity.Service.Repositories;
+
+/// <summary>
+/// Allows for retrieval of RSA keys used for JWT signing and converting them to a desired format.
+/// </summary>
+[ExcludeFromCodeCoverage]
+[SuppressMessage("Design", "CA1024:Use properties where appropriate")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+public class JwtKeysRepository
+{
+    private const string CERT_DIR_NAME = "JWK";
+
+    private readonly Lazy<RSA> rsaKey = new Lazy<RSA>(() => {
+        var rsa = RSA.Create();
+        var fileName = Directory.EnumerateFiles(CERT_DIR_NAME).FirstOrDefault(f => f.EndsWith(".pem", StringComparison.OrdinalIgnoreCase));
+        if (string.IsNullOrEmpty(fileName))
+            throw new InvalidOperationException($"JWK RSA keys need to be stored in the {CERT_DIR_NAME} folder.");
+        var rsaPassword = EnvironmentalVariablesReader.GetOptionalVariable(EnvironmentalVariablesReader.JwtRsaPasswordVariableName);
+        if (string.IsNullOrEmpty(rsaPassword))
+            rsa.ImportFromPem(fileName);
+        else
+            rsa.ImportFromEncryptedPem(fileName, rsaPassword);
+        return rsa;
+    });
+
+    /// <summary>
+    /// Returns a complete RSA key pair value.
+    /// </summary>
+    public RSA GetRsaKeyPair() => rsaKey.Value;
+    /// <summary>
+    /// Returns the public key in a <see cref="RsaSecurityKey"/> format.
+    /// </summary>
+    public RsaSecurityKey GetRsaAsSecurityKey() => new RsaSecurityKey(GetRsaKeyPair().ExportParameters(false));
+    /// <summary>
+    /// Returns the public key as a json web key.
+    /// </summary>
+    public JsonWebKey GetRsaAsJsonWebKey() => JsonWebKeyConverter.ConvertFromRSASecurityKey(GetRsaAsSecurityKey());
+    //todo: this assumes that the RSA key pair was generated like so: "openssl genpkey -algorithm RSA -out private_key.pem -aes256" -- it should somehow select it based on the key itself
+    /// <summary>
+    /// Returns the public RSA key as a signing credential
+    /// </summary>
+    public SigningCredentials GetRsaAsSigningCredentials() => new SigningCredentials(GetRsaAsSecurityKey(), SecurityAlgorithms.RsaSsaPssSha256);
+}
