5cbd8e2755eacbb561b99c917b9adb28ed7e6aaf
Piotr Kowalski <piotr@pkowalski.net>
2023-05-21 10:51:50 +0200

    Rename `Topic` to `Subject`
    You can tell I don't use email much...

--- Rinkudesu.Identity.Service.Email/EmailOptions.cs
+++ Rinkudesu.Identity.Service.Email/EmailOptions.cs
@@ -8,16 +8,16 @@ public class EmailOptions
     public string To { get; private set; }
     public string? Cc { get; private set; }
     public string? Bcc { get; private set; }
-    public string Topic { get; private set; }
+    public string Subject { get; private set; }
     public string Content { get; private set; }
     public bool IsContentHtml { get; private set; }
 
-    public EmailOptions(string to, string topic, string content, bool isHtml = true, string? cc = null, string? bcc = null)
+    public EmailOptions(string to, string subject, string content, bool isHtml = true, string? cc = null, string? bcc = null)
     {
         To = to;
         Cc = cc;
         Bcc = bcc;
-        Topic = topic;
+        Subject = subject;
         Content = content;
         IsContentHtml = isHtml;
     }
