026c3442d0fdb541d97a49c76a875bf207e450ab
Piotr Kowalski <piotr@pkowalski.net>
2023-05-27 19:50:50 +0200

    Add one more value init

--- Rinkudesu.Identity.Service/DataTransferObjects/AccountCreatedDto.cs
+++ Rinkudesu.Identity.Service/DataTransferObjects/AccountCreatedDto.cs
@@ -3,5 +3,5 @@ namespace Rinkudesu.Identity.Service.DataTransferObjects;
 public class AccountCreatedDto
 {
     public Guid UserId { get; set; }
-    public string EmailConfirmationToken { get; set; }
+    public string EmailConfirmationToken { get; set; } = string.Empty;
 }
